2d584c27d377f32cc278262451d37a92d8942136
Alejandro Mathus <aalejandrojoseml94@gmail.com>
2022-06-30 18:31:25 -0300

    DataBinding added

--- app/src/main/java/com/example/myapplication/ui/LoginFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/LoginFragment.kt
@@ -5,11 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentLoginBinding
 
 class LoginFragment: Fragment() {
-     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-         return inflater.inflate(R.layout.fragment_login, container, false)
+    lateinit var binding: FragmentLoginBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
