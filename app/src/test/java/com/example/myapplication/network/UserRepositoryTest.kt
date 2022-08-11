d685c491caba48d1fce80ff52c10cae57238b7b9
Alejandro Mathus <alejandrojoseml94@gmail.com>
2022-08-11 14:55:47 -0300

    UserRepository test added, Mockito broken

--- app/src/test/java/com/example/myapplication/network/UserRepositoryTest.kt
+++ app/src/test/java/com/example/myapplication/network/UserRepositoryTest.kt
@@ -14,28 +14,28 @@ import retrofit2.Response
 
 class UserRepositoryTest {
 
-    // lateinit var userRepository: UserRepository
-    //
-    // @Mock
-    // lateinit var apiService: UserAPI
-    //
-    // @Before
-    // fun setup() {
-    //     MockitoAnnotations.initMocks(this)
-    //     userRepository = UserRepository()
-    //     apiService = ApiBuilder(BuildConfig.WOLOX_API_URL).create(UserAPI::class.java)
-    // }
-    //
-    // @Test
-    // fun `get User Info Response`() {
-    //     runBlocking {
-    //         val userAuth = UserAuth(
-    //             "melvin.lambert15@example.com",
-    //             "123456"
-    //         )
-    //         Mockito.`when`(apiService.getUserInfo(userAuth)).thenReturn(Response.success(USER_INFO))
-    //         val response = userRepository.getUserInfo(userAuth)
-    //         Assertions.assertThat(response).isEqualTo(USER_INFO)
-    //     }
-    // }
+    lateinit var userRepository: UserRepository
+
+    @Mock
+    lateinit var apiService: UserAPI
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        userRepository = UserRepository()
+        apiService = ApiBuilder(BuildConfig.WOLOX_API_URL).create(UserAPI::class.java)
+    }
+
+    @Test
+    fun `get User Info Response`() {
+        runBlocking {
+            val userAuth = UserAuth(
+                "melvin.lambert15@example.com",
+                "123456"
+            )
+            Mockito.`when`(apiService.getUserInfo(userAuth)).thenReturn(Response.success(USER_INFO))
+            val response = userRepository.getUserInfo(userAuth)
+            Assertions.assertThat(response).isEqualTo(USER_INFO)
+        }
+    }
 }
