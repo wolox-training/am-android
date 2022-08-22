d299cdea2afcd4bce84a804e7abc31977a25cd52
Alejandro Mathus <alejandrojoseml94@gmail.com>
2022-08-22 10:15:48 -0300

    Comments erased

--- app/src/main/java/com/example/myapplication/utils/NewsAdapter.kt
+++ app/src/main/java/com/example/myapplication/utils/NewsAdapter.kt
@@ -12,37 +12,25 @@ import com.example.myapplication.R
 class NewsAdapter(private val dataSet: List<String>) :
     RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
 
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder).
-     */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView
 
         init {
-            // Define click listener for the ViewHolder's View.
             textView = view.findViewById(R.id.news_body)
         }
     }
 
-    // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        // Create a new view, which defines the UI of the list item
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.news_item_holder, viewGroup, false)
 
         return ViewHolder(view)
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
 
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
         viewHolder.textView.text = dataSet[position]
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = dataSet.size
-
 }
